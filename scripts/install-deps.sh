73e2ec8b56c4523caba5d679406d3b8d441cea0d
cyrilloo <walloiscyril@gmail.com>
2017-05-09 20:12:16 +0200

    Fix permission in install-deps.sh
    Add sudo in last 2 command:
    virtualenv --system-site-packages -p python3 env
    env/bin/pip install google-assistant-sdk[auth_helpers]==0.1.0 \
      grpc-google-cloud-speech-v1beta1==0.14.0 protobuf==3.1.0

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -31,6 +31,6 @@ sudo apt-get -y install -t stretch python3-httplib2 python3-configargparse
 sudo pip3 install --upgrade pip virtualenv
 
 cd "${scripts_dir}/.."
-virtualenv --system-site-packages -p python3 env
-env/bin/pip install google-assistant-sdk[auth_helpers]==0.1.0 \
+sudo virtualenv --system-site-packages -p python3 env
+sudo env/bin/pip install google-assistant-sdk[auth_helpers]==0.1.0 \
   grpc-google-cloud-speech-v1beta1==0.14.0 protobuf==3.1.0
