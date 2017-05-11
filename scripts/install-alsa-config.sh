330dc967c599b52afbcb467be1a085e251252fa5
Stefan Sauer <ensonic@google.com>
2017-05-11 10:59:14 +0200

    Update setup scripts.
    Add UID checks. Remove extra sudo calls. Add copyright header.

--- scripts/install-alsa-config.sh
+++ scripts/install-alsa-config.sh
@@ -18,6 +18,11 @@
 
 set -o errexit
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)" 1>&2
+   exit 1
+fi
+
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 asoundrc=/home/pi/.asoundrc
