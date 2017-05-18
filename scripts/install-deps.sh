115e4d5e93c0e385f88d71f7b5026460cdf0e0a4
Rodrigo Queiro <rodrigoq@google.com>
2017-05-18 16:34:27 +0200

    Determine the owner of the directory with `ls`
    This fixes #38 by making sure we create the virtualenv with the right
    owner.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -13,17 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-RUN_AS="pi"
-
 set -o errexit
 
+scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+# make sure we're running as the owner of the checkout directory
+RUN_AS="$(ls -ld "$scripts_dir" | awk 'NR==1 {print $3}')"
 if [ "$USER" != "$RUN_AS" ]
 then
     echo "This script must run as $RUN_AS, trying to change user..."
     exec sudo -u $RUN_AS $0
 fi
-
-scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
 
 sudo apt-get -y install alsa-utils python3-all-dev python3-pip python3-numpy \
   python3-scipy python3-virtualenv python3-rpi.gpio python3-pysocks \
