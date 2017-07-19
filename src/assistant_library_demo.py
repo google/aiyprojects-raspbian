c5c6c76181499a514d661e4006ec4e1cc2998337
Fan Deng <fandeng@google.com>
2017-07-19 11:31:38 -0700

    Also press the button to start a conversation.
    This change enabled the GPIO trigger (button) to be used with the
    assistant_library_demo. Now the user can either say the hot word "Okay,
    Google" or press the VoiceHat arcade button to start a conversation.

--- src/assistant_library_demo.py
+++ src/assistant_library_demo.py
@@ -16,54 +16,75 @@
 """A demo of the Google Assistant Library"""
 
 import sys
+import threading
 
 import aiy.assistant.auth_helpers
 import aiy.voicehat
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+
+
+class MyAssistant(object):
+    def __init__(self, assistant):
+        self._assistant = assistant
+        self._ready = False
+        self._status_ui = aiy.voicehat.get_status_ui()
+        self._recognizer = threading.Thread(target=self._run_recognizer)
+
+    def start(self):
+        self._recognizer.start()
+
+    def _run_recognizer(self):
+        for event in self._assistant.start():
+            self._process_event(event)
+
+    def _process_event(self, event):
+        if event.type == EventType.ON_START_FINISHED:
+            self._status_ui.status('ready')
+            self._ready = True
+            aiy.voicehat.get_button().on_press(self._on_button_press)
+            if sys.stdout.isatty():
+                print('Say "OK, Google" or press the VoiceHat button then speak,'
+                      'or press Ctrl+C to quit...')
+
+        elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+            self._ready = False
+            self._status_ui.status('listening')
+
+        elif event.type == EventType.ON_END_OF_UTTERANCE:
+            self._status_ui.status('thinking')
+
+        elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
+            self._status_ui.status('ready')
+            self._ready = True
+
+        elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+            sys.exit(1)
+
+    def _on_button_press(self):
+        print('on_button_press')
+        if self._ready:
+            self._ready = False
+            self._assistant.start_conversation()
 
 
 def main():
     """Run a recognizer using the Google Assistant Library.
 
     The Google Assistant Library has direct access to the audio API, so this
-    Python code doesn't need to record audio.
-    Hot word detection "OK, Google" is supported.
+    Python code doesn't need to record audio. Hot word detection "OK, Google" is
+    supported.
+
+    The Google Assistant Library can be installed with:
+    env/bin/pip install google-assistant-library==0.0.2
+
+    It is available for Raspberry Pi 2/3 only.
     """
-
-    try:
-        from google.assistant.library import Assistant
-        from google.assistant.library.event import EventType
-    except ImportError:
-        print('''
-ERROR: failed to import the Google Assistant Library. This is required for
-"OK Google" hotwording, but is only available for Raspberry Pi 2/3. It can be
-installed with:
-    env/bin/pip install google-assistant-library==0.0.2''')
-        sys.exit(1)
-
     credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
-
-    def process_event(event):
-        status_ui = aiy.voicehat.get_status_ui()
-        if event.type == EventType.ON_START_FINISHED:
-            status_ui.status('ready')
-            if sys.stdout.isatty():
-                print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
-
-        elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-            status_ui.status('listening')
-
-        elif event.type == EventType.ON_END_OF_UTTERANCE:
-            status_ui.status('thinking')
-
-        elif event.type == EventType.ON_CONVERSATION_TURN_FINISHED:
-            status_ui.status('ready')
-
-        elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-            sys.exit(1)
-
     with Assistant(credentials) as assistant:
+        my_assistant = MyAssistant(assistant)
         for event in assistant.start():
-            process_event(event)
+            my_assistant._process_event(event)
 
 
 if __name__ == '__main__':
