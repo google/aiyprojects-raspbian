c5745ec6cbde0a2269ca2c6e2ba02f0538bbc926
Dmitry Kovalev <dkovalev@google.com>
2018-02-12 11:15:53 -0800

    Update LEDs example code.
    Change-Id: I44ea6c8a7d1e5433fff0668be54f8c95852f114c

--- src/examples/vision/leds_example.py
+++ src/examples/vision/leds_example.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import math
 import time
 
 from aiy.vision.leds import Leds
@@ -27,86 +28,124 @@ PURPLE = (0xFF, 0x00, 0xFF)
 CYAN = (0x00, 0xFF, 0xFF)
 WHITE = (0xFF, 0xFF, 0xFF)
 
+def blend(color_a, color_b, alpha):
+    return tuple([math.ceil(alpha * color_a[i] + (1.0 - alpha) * color_b[i]) for i in range(3)])
+
 leds = Leds()
 
-# Iterate over different colors
-for rgb in (RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE):
-    leds.update(Leds.rgb_on(rgb))
-    time.sleep(1)
+print('RGB: Solid RED for 1 second')
+leds.update(Leds.rgb_on(RED))
+time.sleep(1)
 
-# Turn rgb off
+print('RGB: Solid GREEN for 1 second')
+leds.update(Leds.rgb_on(GREEN))
+time.sleep(1)
+
+print('RGB: Solid YELLOW for 1 second')
+leds.update(Leds.rgb_on(YELLOW))
+time.sleep(1)
+
+print('RGB: Solid BLUE for 1 second')
+leds.update(Leds.rgb_on(BLUE))
+time.sleep(1)
+
+print('RGB: Solid PURPLE for 1 second')
+leds.update(Leds.rgb_on(PURPLE))
+time.sleep(1)
+
+print('RGB: Solid CYAN for 1 second')
+leds.update(Leds.rgb_on(CYAN))
+time.sleep(1)
+
+print('RGB: Solid WHITE for 1 second')
+leds.update(Leds.rgb_on(WHITE))
+time.sleep(1)
+
+print('RGB: Off for 1 second')
 leds.update(Leds.rgb_off())
+time.sleep(1)
 
-# Blink privacy led 3 times
 for _ in range(3):
+    print('Privacy: On (brightness=default)')
     leds.update(Leds.privacy_on())
     time.sleep(1)
+    print('Privacy: Off')
     leds.update(Leds.privacy_off())
     time.sleep(1)
 
-# Blink privacy led 3 times with reduced brightness
 for _ in range(3):
+    print('Privacy: On (brightness=5)')
     leds.update(Leds.privacy_on(5))
     time.sleep(1)
+    print('Privacy: Off')
     leds.update(Leds.privacy_off())
     time.sleep(1)
 
-# Blink one time two times per second (period=500ms, 2Hz)
+print('Set blink pattern: period=500ms (2Hz)')
 leds.pattern = Pattern.blink(500)
 
-# Only red led
+print('RGB: Blink RED for 5 seconds')
 leds.update(Leds.rgb_pattern(RED))
 time.sleep(5)
 
-# Only green led
+print('RGB: Blink GREEN for 5 seconds')
 leds.update(Leds.rgb_pattern(GREEN))
 time.sleep(5)
 
-# Only blue led
+print('RGB: Blink BLUE for 5 seconds')
 leds.update(Leds.rgb_pattern(BLUE))
 time.sleep(5)
 
-# Change pattern and blink one time per second (period=1000ms, 1Hz)
+print('Set breathe pattern: period=1000ms (1Hz)')
 leds.pattern = Pattern.breathe(1000)
 
-# Only red led
+print('RGB: Breathe RED for 5 seconds')
 leds.update(Leds.rgb_pattern(RED))
 time.sleep(5)
 
-# Only green led
+print('RGB: Breathe GREEN for 5 seconds')
 leds.update(Leds.rgb_pattern(GREEN))
 time.sleep(5)
 
-# Only blue led
+print('RGB: Breathe BLUE for 5 seconds')
 leds.update(Leds.rgb_pattern(BLUE))
 time.sleep(5)
 
-# Manually increase red led brightness
+print('RGB: Increase RED brightness for 3.2 seconds')
 for i in range(32):
     leds.update(Leds.rgb_on((8 * i, 0, 0)))
     time.sleep(0.1)
 
-# Manually decrease red led brightness
+print('RGB: Decrease RED brightness for 3.2 seconds')
 for i in reversed(range(32)):
     leds.update(Leds.rgb_on((8 * i, 0, 0)))
     time.sleep(0.1)
 
-# Turn on privacy led for 2 seconds
+print('RGB: Blend between GREEN and BLUE for 3.2 seconds')
+for i in range(32):
+    leds.update(Leds.rgb_on(blend(BLUE, GREEN, i / 32)))
+    time.sleep(0.1)
+
+print('RGB: Off for 1 second')
+leds.update(Leds.rgb_off())
+time.sleep(1)
+
+print('Privacy: On for 2 seconds')
 with PrivacyLed(leds):
     time.sleep(2)
 
-# Turn on rgb leds for 2 seconds
-with RgbLeds(leds, Leds.rgb_on(RED)):
+print('RGB: Solid GREEN for 2 seconds')
+with RgbLeds(leds, Leds.rgb_on(GREEN)):
     time.sleep(2)
 
-# Custom configuration for each channel
+print('Custom configuration for 5 seconds')
 leds.update({
-    1: Leds.Channel(Leds.Channel.PATTERN, 128),
-    2: Leds.Channel(Leds.Channel.OFF, 0),
-    3: Leds.Channel(Leds.Channel.ON, 128),
-    4: Leds.Channel(Leds.Channel.PATTERN, 64),
+    1: Leds.Channel(Leds.Channel.PATTERN, 128),  # Red channel
+    2: Leds.Channel(Leds.Channel.OFF, 0),        # Green channel
+    3: Leds.Channel(Leds.Channel.ON, 128),       # Blue channel
+    4: Leds.Channel(Leds.Channel.PATTERN, 64),   # Privacy channel
 })
 time.sleep(5)
 
-# Reset and finish
+print('Done')
 leds.reset()
