cbdca175f5fcae55ca125e0a1ee861292c29e352
Nick Lee <lee1nick@yahoo.ca>
2018-03-16 17:21:50 +0800

    Removed unwanted stuff

--- src/aiy/vision/models/utils.py
+++
@@ -1,9 +0,0 @@
-"""Utility to load compute graphs from diffrent sources."""
-
-import os
-
-
-def load_compute_graph(name):
-    path = os.environ.get('VISION_BONNET_MODELS_PATH', '/opt/aiy/models')
-    with open(os.path.join(path, name), 'rb') as f:
-        return f.read()
