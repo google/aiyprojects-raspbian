e7874d295728a1b08c7c6bd404bbf5fcd079d259
Dmitry Kovalev <dkovalev@google.com>
2018-07-04 23:49:39 -0700

    Get rid of custom debounce code.
    Change-Id: Ib2e65bec3ea9f7478333b8f0c0820c9ecf540a76

--- src/aiy/_drivers/_button.py
+++ src/aiy/_drivers/_button.py
@@ -21,10 +21,7 @@ import RPi.GPIO as GPIO
 class Button(object):
     """Detect edges on the given GPIO channel."""
 
-    def __init__(self,
-                 channel,
-                 polarity=GPIO.FALLING,
-                 pull_up_down=GPIO.PUD_UP,
+    def __init__(self, channel, polarity=GPIO.FALLING, pull_up_down=GPIO.PUD_UP,
                  debounce_time=0.08):
         """A simple GPIO-based button driver.
 
@@ -45,13 +42,10 @@ class Button(object):
 
         self.channel = int(channel)
         self.polarity = polarity
-        self.expected_value = polarity == GPIO.RISING
         self.debounce_time = debounce_time
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(channel, GPIO.IN, pull_up_down=pull_up_down)
-
-        self.callback = None
 
     def __del__(self):
         GPIO.cleanup(self.channel)
@@ -61,12 +55,12 @@ class Button(object):
 
         This method blocks until the button is pressed.
         """
-        GPIO.add_event_detect(self.channel, self.polarity)
+        expected = True if self.polarity == GPIO.RISING else False
         while True:
-            if GPIO.event_detected(self.channel) and self._debounce():
-                GPIO.remove_event_detect(self.channel)
-                return
-            time.sleep(0.02)
+            GPIO.wait_for_edge(self.channel, self.polarity)
+            if GPIO.input(self.channel) == expected:
+                time.sleep(self.debounce_time)
+                break
 
     def on_press(self, callback):
         """Call the callback whenever the button is pressed.
@@ -81,25 +75,13 @@ class Button(object):
               print "button pressed: channel = %d" % channel
           my_button.on_press(MyButtonPressHandler)
         """
+        expected = True if self.polarity == GPIO.RISING else False
+
+        def gpio_callback(channel):
+            if GPIO.input(self.channel) == expected:
+                callback()
+
         GPIO.remove_event_detect(self.channel)
         if callback:
-            self.callback = callback
-            GPIO.add_event_detect(
-                self.channel, self.polarity, callback=self._debounce_and_callback)
-
-    def _debounce_and_callback(self, _):
-        if self._debounce():
-            self.callback()
-
-    def _debounce(self):
-        """Debounce the GPIO signal.
-
-        Check that the input holds the expected value for the debounce
-        period, to avoid false trigger on short pulses.
-        """
-        start = time.time()
-        while time.time() < start + self.debounce_time:
-            if GPIO.input(self.channel) != self.expected_value:
-                return False
-            time.sleep(0.01)
-        return True
+            GPIO.add_event_detect(self.channel, self.polarity, callback=gpio_callback,
+                bouncetime=int(self.debounce_time * 1000))
