f76880569d77e97de9a510c1fd7df3f280a619d8
Dmitry Kovalev <dkovalev@google.com>
2018-11-15 14:40:29 -0800

    Update library dependencies.
    Change-Id: I1e1857eb3d321404076126399c50f7ce46d2b579

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiy-projects-python',
-    version='1.1',
+    version='1.2',
     description='AIY Python API',
     author='AIY Team',
     author_email='support-aiyprojects@google.com',
@@ -14,14 +14,17 @@ setup(
     license='Apache 2',
     packages=find_packages(),
     install_requires=[
-        'google-assistant-grpc==0.1.0',
-        'google-assistant-library==0.1.0',
-        'google-cloud-speech==0.30.0',
+        'google-assistant-library==1.0.1',
+        'google-assistant-grpc==0.2.0',
+        'google-auth==1.5.1',
+        'google-auth-oauthlib==0.2.0',
+        'google-cloud-speech==0.36.0',
         'gpiozero',
         'paho-mqtt',
+        'protobuf==3.6.1',
         'picamera',
         'pillow',
         'RPi.GPIO',
     ],
-    python_requires='>=3.5.0',
+    python_requires='>=3.5.3',
 )
